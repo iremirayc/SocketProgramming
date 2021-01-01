204ee134ada52c82234de4c80f0265f4f5f24f87
iremmiraycakir <iremiraycakir@gmail.com>
2021-01-01 22:16:13 +0300

    Video bitti.

--- server.py
+++ server.py
@@ -2,46 +2,43 @@ import socket
 import threading
 
 HEADER = 64
-PORT = 8080 #  ?
-#SERVER = "192.168.1.4"
-SERVER = socket.gethostbyname(socket.gethostname()) # get IP address automatically
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
-#print(SERVER)
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create new socket
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
-# handle all communication between client and server
-def handle_client(connection, addr):
+
+def handle_client(conn, addr):
     print(f"[NEW CONNECTION] {addr} connected.")
 
     connected = True
     while connected:
-        message_length = connection.recv(HEADER).decode(FORMAT)
-        message_length = int(message_length)
-        message = connection.recv(message_length).decode(FORMAT)
-        if message == DISCONNECT_MESSAGE:
-            connected = False
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
 
-        print(f"[{addr}] {message}")
+            print(f"[{addr}] {msg}")
+            conn.send("Msg received".encode(FORMAT))
 
-    connection.close()
+    conn.close()
+
 
 def start():
-    server.listen(1)
-    print (f"[LISTENING] Server is listening on {SERVER}")
+    server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
     while True:
-        connection, addr = server.accept()
-        thread = threading.Thread(thread = handle_client, args = (connection, addr))
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
-        print (f"[ACTIVE CONNECTIONS {threading.activeCount() - 1}")  # ??
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
 
 
-
-print(f"[STARTING] server is starting...")
+print("[STARTING] server is starting...")
 start()
-
-
-
